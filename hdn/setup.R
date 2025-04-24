7d9f4c3fb095c82a558874d7ad0f4ca665b31ad8
Chloe Pou-Prom <PouPromC@workbench4.lxd>
2025-04-24 17:36:54 -0400

    update setup

--- hdn/setup.R
+++ hdn/setup.R
@@ -2,6 +2,9 @@
 # You can run this line by line by pressing Ctrl + Enter on the selected line
 # Or press on 'Source' button in the top right
 
+# Rendering
+install.packages(c("rmarkdown", "markdown"))
+
 # Data processing
 install.packages(c("dplyr", "ggplot2", "knitr", "tidyr", "DT"))
 
